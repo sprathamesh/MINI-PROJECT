b300decec15a7f830aa91c71deb6aeb822eb6e0d
sprathamesh <89856720+sprathamesh@users.noreply.github.com>
2022-11-02 16:10:29 +0530

    Update GUESSING_NUM_GAME.c

--- GUESSING_NUM_GAME.c
+++ GUESSING_NUM_GAME.c
@@ -2,12 +2,13 @@
 //number = ran()%100 + 1;
 //this work as any random number will divide by 100 the answer will be in 0 to 100 
 
+
 #include<stdio.h>
 #include<stdlib.h>                          // To use rand() function
 #include<time.h>                            // To use time(0) function
 int main()
 {
-	int number, guess, nguesses=1;
+	int number, guess, nguesses=1;      // guess is number that is generated &nguesses is used for no of time you guessed the number while playing
 	srand(time(0));                     //random numbers for every output
 	number = rand()%100 + 1;            //generate random number between 1 to 100 if we take 200 instead of 100 number will generate number between 1 to 200.
     //printf("the number is %d",number);
